840e48bb6daeb21662ceabad085a51a3a6d0f8b8
tzecris <tzecris13@gmail.com>
2020-05-12 22:18:09 +0300

    product carousel

--- src/components/products/ProductCarousel.js
+++ src/components/products/ProductCarousel.js
@@ -13,19 +13,23 @@ function ProductCarousel() {
         <Carousel activeIndex={index} onSelect={handleSelect} slide={false} className="container">
             <Carousel.Item>
                 <div className="row">
-                    <ProductCard className="col-md-2" />
-                    <ProductCard className="col-md-2" />
-                    <ProductCard className="col-md-2" />
-                    <ProductCard className="col-md-2" />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
                 </div>
-                <ProductCard />
                 <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <ProductCard />
+                <div className="row">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
 
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
@@ -33,49 +37,12 @@ function ProductCarousel() {
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <div class="col-md-3" style={{ float: 'left' }}>
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                            src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap" />
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-              card's content.</p>
-                            <a class="btn btn-primary">Button</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3" style={{ float: 'left' }}>
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                            src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap" />
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-              card's content.</p>
-                            <a class="btn btn-primary">Button</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3" style={{ float: 'left' }}>
+                <div className="row">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
                     <ProductCard />
                 </div>
-
-                <div class="col-md-3" style={{ float: 'left' }}>
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                            src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap" />
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-              card's content.</p>
-                            <a class="btn btn-primary">Button</a>
-                        </div>
-                    </div>
-                </div>
-
             </Carousel.Item>
         </Carousel>
     );
